936f7c6f903fccbfbdbcd0abf07e3c7a426a2dbf
sung_kyuk <sung_kyuk@hotmail.com>
2018-01-02 19:43:49 -0800

    updated

--- calc_perm.js
+++ calc_perm.js
@@ -1,42 +1,96 @@
-var markerTable = new Array();
 function calc_perm_begin(input) {
-	marker = new Array(input.length);
+	marker = markerTable;
 	result = new Array();
-	calc_perm(input);
+	resultArray = new Array();
+	calc_perm();
 }
 
 var f = function condition(o) {
 	return true;
 }
 
-function permCond() {
-	return result.length == input.legnth;
-}
+function calc_perm() {
 
-function calc_perm(input) {
+	// All length permutation. Valid if it's draws cycle. Once cycle is found, terminate the search.
+	if ((result.length > 0) && result[result.length-1].end == result[0].start) { 
+		if (result.length >= 3) { // only consider longer than 3 
+			// deep clone the result and store 
+			//resultArray.push(JSON.stringify(result));
+			var contentDiv = document.getElementById('content');
 
-	if (permCond()) {
-		alert(result);
+			var pStr = '<p>';	
+			var price = 1;
+			for (var i = 0; i < result.length; i++) {
+				price /= result[i].price;
+				pStr += result[i].start + '(' + 1/result[i].price + ')' + ' -> ';
+			}
+			
+			pStr += result[i-1].end;
+			pStr += ' = ' + price + '</p>';
+			contentDiv.innerHTML += pStr;
+		}
 		return;
 	}
-	
+
 	var next = 0;
 
-	while ((next = nextElemAfter(next, f, input)) >= 0) {
-		marker[next] = 1;
-		result.push(input[next]);
-		calc_perm(input);
-		result = result.slice(0, result.length -1);
-		marker[next] = undefined;
-		next++;
-	}
+	if ((next = nextElemAfter(next, f)) < 0)
+		return;
+
+	do {
+		var nextIdx = Math.floor(next);
+		var nextStat = (next * 10) % 10;
+
+		if (marker[nextIdx].stat >0) { // cycle found. skip
+			continue;
+		}
+		marker[nextIdx].stat += nextStat;
+
+		//create reocrd to put in to the result
+		var rec = new Object();
+
+		if (nextStat & 1 > 0) { 
+			rec.start = marker[nextIdx].elem.unit;
+			rec.end = marker[nextIdx].elem.tag;
+			rec.price = marker[nextIdx].elem.price;
+
+		} else if (marker[nextIdx].elem.reverse && (nextStat & 2) > 0) { // reverse case
+			rec.start = marker[nextIdx].elem.tag;
+			rec.end = marker[nextIdx].elem.unit;
+			rec.price = 1/marker[nextIdx].elem.price;
+
+		} else {
+			//TODO: handle invalid state
+		}
+
+		result.push(rec);
+
+		calc_perm();
+
+		result.pop();
+		marker[nextIdx].stat -= nextStat;
+	} while ((next = nextElemAfter(next, f)) >= 0) ;
 }
 
-function nextElemAfter(idx, isTrue, input) {
-	for (var i = idx; i< input.length; i++) {
-		if ( marker[i] == undefined && isTrue(input[i])) {
-			return i;
+function nextElemAfter(idx, isTrue) {
+
+	var i = Math.floor(idx);
+	var minor = (idx*10)%10
+
+	for (; i< markerTable.length; i++) {
+
+		if ((marker[i].stat & 1) == 0 && (minor < 1)) { // regular not set
+			if ((result.length ==0) || result[result.length-1].end == marker[i].elem.unit) // link found
+				return i + 0.1;
 		}
+
+		if ((marker[i].stat & 2) == 0 && (minor < 2)) { // reverse not set
+			if (marker[i].elem.reverse 
+				&& ((result.length == 0) || result[result.length-1].end == marker[i].elem.tag)) // link found
+				return i + 0.2;
+			
+		} 
+		minor = 0;
 	}
 	return -1;
 }
--- tool.js
+++ tool.js
@@ -1,79 +1,94 @@
 // sample data structure
 var exchangeList = {};
 var row = {};
-
+var markerTable = new Array();
 
 // Read from currency exchange
+exchangeList['WON-DOLLOR'] = new Object();
 exchangeList['WON-DOLLOR'].currency = 'USD';
 exchangeList['WON-DOLLOR'].fee= 0.6;
 exchangeList['WON-DOLLOR'].records = row;
 
 row['KRW'] = 
 {
+	tag: 'KRW',
 	index: 0,
-	price: 1061,
+	price: 0.00094,
 	unit: 'USD',
 	reverse: true,
 	refs:[]
 };
 
+markerTable.push({
+stat: 0,
+elem: row['KRW']
+});
 
 // Read data from Coinbase
 var row = {};
 
+exchangeList['Coinbase'] = new Object();
 exchangeList['Coinbase'].endpoint ='';
-exchangeList['Coinbase'].['USD'].fee = 0.6;
-exchangeList['Coinbase'].['USD'].records = row;
+
+exchangeList['Coinbase']['USD'] = new Object();
+exchangeList['Coinbase']['USD'].fee = 0.6;
+exchangeList['Coinbase']['USD'].records = row;
 
 row['BTC'] = {
+	tag: 'BTC',
 	index: 1, // increment 
 	price: 14330.24,
-	unit :'USD'
+	unit :'USD',
 	reverse: true,
 	refs: []
 	
 };
 
-markerTable.push(new Object() {
-this.stat = 0;
-this.elem = row['BTC'];
+markerTable.push({
+stat: 0,
+elem: row['BTC']
 });
 
 
 // Read data from Bithumb
-row = {};
-
-row['BTC'] = 
-{
-	index: 2,
-	price: 19000000.0,
-	unit: 'KRW',
-	reverse: true,
-	refs:[]
-};
-
-markerTable.push(new Object() {
-this.stat = 0;
-this.elem = row['BTC'];
-});
-
-row['RP'] = 
-{
-	index: 3,
-	price: 3000.0,
-	unit: 'KRW',
-	reverse: true,
-	refs:[]
-};
-
-markerTable.push(new Object() {
-this.stat = 0;
-this.elem = row['RP'];
-});
-
-exchangeList['Bithum'].endpoint = '';
-exchangeList['Bithum']['KRW'].fee = 0.6;
-exchangeList['Bithum']['KRW'].records = row;
+//row = {};
+//
+//row['BTC'] = 
+//{
+//	tag: 'BTC',
+//	index: 2,
+//	price: 19000000.0,
+//	unit: 'KRW',
+//	reverse: true,
+//	refs:[]
+//};
+//
+//markerTable.push({
+//stat: 0,
+//elem: row['BTC']
+//});
+//
+//row['RP'] = 
+//{
+//	tag: 'RP',
+//	index: 3,
+//	price: 3000.0,
+//	unit: 'KRW',
+//	reverse: true,
+//	refs:[]
+//};
+//
+//markerTable.push({
+//stat: 0,
+//elem: row['RP']
+//});
+//
+//exchangeList['Bithum'] = new Object();
+//exchangeList['Bithum'].endpoint = '';
+//
+//exchangeList['Bithum']['KRW'] = new Object();
+//exchangeList['Bithum']['KRW'].fee = 0.6;
+//exchangeList['Bithum']['KRW'].records = row;
 
 // read from Poloniex
 //row = {};
@@ -111,5 +126,4 @@ exchangeList['Bithum']['KRW'].records = row;
 //	fee: 0.6,
 //	refs:[]
 //};
-
 
